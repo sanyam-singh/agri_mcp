89875460ae12c589ab3f784a17b9ea1dbb766656
sanyam-singh <sanyam@digitalgreen.org>
2025-06-16 16:16:15 +0530

    Added code for MCP

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-# Remove: from starlette.graphql import GraphQLApp
-from graphene_starlette.fastapi import GraphQLApp # Add this line
+# Remove: from starlette.graphql import GraphQLApp# Add this line
+
+from starlette_graphene import GraphQLApp
 from graphql_app.schema import schema
 
 app = FastAPI()
